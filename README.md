a87b1a5e69ee7fabf93cda42acfb81e626386e25
K1GOL <ossi.lamsa@gmail.com>
2022-11-09 23:06:35 +0200

    1.4.1

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 rtcbeam is an end-to-end encrypted peer-to-peer file transfer app powered by WebRTC. The app can be used live at [https://rtc-beam.web.app](https://rtc-beam.web.app). All file transfers are encrypted by default, and the broker server can be any freely configured PeerJS PeerServer.
 
-Licensed under the BSD-2-Clause license.
+This is a web-based client, for core functionality module, see [rtcbeam-core](https://github.com/K1GOL/rtcbeam-core).
+
+Licensed under the BSD 2-Clause license.
 
 ## Project setup
 ```
--- src/components/StatusLabel.vue
+++ src/components/StatusLabel.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="my-6 mx-auto text-2xl">
     <p>{{ store.appStatus }}</p>
-    <p class="text-sm">rtcbeam v{{ store.version }} | 2022 K1GOL | <u><a href="https://github.com/K1GOL/rtcbeam">https://github.com/K1GOL/rtcbeam</a></u></p>
+    <p class="text-sm">Web client {{ store.version }} | Core {{ core.getVersion() }} | 2022 K1GOL | <u><a href="https://github.com/K1GOL/rtcbeam">https://github.com/K1GOL/rtcbeam</a></u></p>
   </div>
 </template>
 
 <script>
 
 import { store } from '../store.js'
+import * as core from '../rtcbeam-core/index.js'
 
 export default {
   name: 'StatusLabel',
   data () {
     return {
-      store
+      store,
+      core
     }
   }
 }
